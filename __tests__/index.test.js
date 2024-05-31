1703d2de0283edf37075ee13dc22d2388d90c5ed
Ilya Roschikhmarov <ilrosch@mail.ru>
2024-05-31 17:50:58 +0300

    step 3

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,7 +8,7 @@ describe('Test search function: ', () => {
   test('should output the id of the document in which the element was found', () => {
     const docs = [
       { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" },
-      { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." },
+      { id: 'doc2', text: "Don't shoot that thing at me." },
       { id: 'doc3', text: "I'm your shooter." },
     ];
 
@@ -22,5 +22,15 @@ describe('Test search function: ', () => {
 
     expect(search(docs, 'pint')).toEqual(['doc1']);
     expect(search(docs, 'pint!')).toEqual(['doc1']);
+  });
+
+  test('should use the relevance metric', () => {
+    const docs = [
+      { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" },
+      { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." },
+      { id: 'doc3', text: "I'm your shooter." },
+    ];
+
+    expect(search(docs, 'shoot')).toEqual(['doc2', 'doc1']);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 const search = (docs, token) => {
   const term = token.match(/\w+/g);
   const regexp = new RegExp(`\\b${term}\\b`, 'i');
-  return docs.reduce((acc, item) => (item.text.match(regexp) ? [...acc, item.id] : acc), []);
+  const search = docs.reduce((acc1, item) => {
+    const { id } = item;
+    const count = item.text.split(' ').reduce((acc2, el) => el.match(regexp) ? acc2 + 1 : acc2, 0);
+    return [...acc1, { id, count }];
+  }, [])
+    .filter(({ count }) => count !== 0)
+    .sort((a, b) => {
+      const countA = a.count;
+      const countB = b.count;
+
+      if (countA > countB) {
+        return -1;
+      }
+
+      if (countA < countB) {
+        return 1;
+      }
+
+      return 0;
+    })
+    .map(({ id }) => id);
+
+  return search;
 };
 
 export default search;
